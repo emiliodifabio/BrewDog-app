d26a064b1ef78b09916f45c81b48b9cd8d5263d5
emiliodifabio <emiliodifabio@me.com>
2022-09-28 19:32:09 +0200

    Implemented markup recipe and render recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,6 @@
 import { container, punkapi } from "./config";
 
-const recipeContainer = document.querySelector("beer");
+const recipeContainer = document.querySelector(".beer");
 
 const timeout = function (s) {
   return new Promise((_, reject) => {
@@ -11,6 +11,7 @@ const timeout = function (s) {
 };
 
 const showRecipe = async function () {
+  // Loading recipe
   try {
     const res = await fetch("https://api.punkapi.com/v2/beers?ids=88");
     // const res = await fetch("https://api.punkapi.com/v2/beers?search=pasta");
@@ -29,12 +30,12 @@ const showRecipe = async function () {
       basics: {
         abv: recipe.abv,
         volume: {
-          value: recipe.volume.value,
-          unit: recipe.volume.unit,
+          eu: recipe.volume.value,
+          us: Math.round(recipe.volume.value / 3.785),
         },
         boilVolume: {
-          value: recipe.boil_volume.value,
-          unit: recipe.boil_volume.unit,
+          eu: recipe.boil_volume.value,
+          us: Math.round(recipe.boil_volume.value / 3.785),
         },
         fg: recipe.target_fg,
         og: recipe.target_og,
@@ -50,13 +51,225 @@ const showRecipe = async function () {
         hops: recipe.ingredients.hops,
         yeast: recipe.ingredients.yeast,
       },
-      method: recipe.method,
+      method: {
+        mashTemp: {
+          eu: recipe.method.mash_temp[0].temp.value,
+          us: (recipe.method.mash_temp[0].temp.value * 9) / 5 + 32,
+        },
+        fermentation: {
+          eu: recipe.method.fermentation.temp.value,
+          us: Math.round((recipe.method.fermentation.temp.value * 9) / 5 + 32),
+        },
+      },
       tip: recipe.brewers_tips,
       food: recipe.food_pairing,
     };
     console.log(recipe);
+    const food = recipe.food.join('; ');
+
+    // Rendering recipe
+    const markup = `
+    <div class="beer-info">
+            <h1 class="beer-info__name">${recipe.title}</h1>
+            <h2 class="beer-info__brewed">First Brewed ${
+              recipe.firstBrewed
+            }</h2>
+            <div class="beer-info-flex">
+              <h3 class="beer-info__tagline">${recipe.tagline}</h3>
+              <div class="beer-info--abv-ibu-og-w">
+                <div class="beer-info--abv-w">
+                  <h4 class="beer-info--abv">Abv:</h4>
+                  <h4 class="beer-info--abv-val">${recipe.basics.abv}%</h4>
+                </div>
+                <div class="beer-info--ibu-w">
+                  <h4 class="beer-info--ibu">Ibu:</h4>
+                  <h4 class="beer-info--ibu-val">${recipe.basics.ibu}</h4>
+                </div>
+                <div class="beer-info--og-w">
+                  <h4 class="beer-info--og">Og:</h4>
+                  <h4 class="beer-info--og-val">${recipe.basics.og}</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="cards-w">
+            <div class="cards__description">
+              <h5 class="cards__description--title">The beer is...</h5>
+              <p class="cards__description--text">
+              ${recipe.description}
+              </p>
+            </div>
+            <div class="cards__basics">
+              <h5 class="cards__basics--title">Basics</h5>
+              <div class="cards__basics--unit-details-w">
+                <p class="cards__basics--unit-details">Volume</p>
+                <p class="cards__basics--unit-details">Boil Volume</p>
+                <p class="cards__basics--unit-details">Abv</p>
+                <p class="cards__basics--unit-details">Target Fg</p>
+                <p class="cards__basics--unit-details">Target OG</p>
+                <p class="cards__basics--unit-details">Ebc</p>
+                <p class="cards__basics--unit-details">Srm</p>
+                <p class="cards__basics--unit-details">Ph</p>
+                <p class="cards__basics--unit-details">Attenuation Level</p>
+              </div>
+              <div class="cards__basics--unit-EU-w">
+                <p class="cards__basics--unit-EU">${
+                  recipe.basics.volume.eu
+                }l</p>
+                <p class="cards__basics--unit-EU">${
+                  recipe.basics.boilVolume.eu
+                }l</p>
+              </div>
+              <div class="cards__basics--unit-US-w">
+                <p class="cards__basics--unit-US">${
+                  recipe.basics.volume.us
+                }gal</p>
+                <p class="cards__basics--unit-US">${
+                  recipe.basics.boilVolume.us
+                }gal</p>
+                <p class="cards__basics--unit-US">${recipe.basics.abv}%</p>
+                <p class="cards__basics--unit-US">${recipe.basics.fg}</p>
+                <p class="cards__basics--unit-US">${recipe.basics.og}</p>
+                <p class="cards__basics--unit-US">${recipe.basics.ebc}</p>
+                <p class="cards__basics--unit-US">${recipe.basics.srm}</p>
+                <p class="cards__basics--unit-US">${recipe.basics.ph}</p>
+                <p class="cards__basics--unit-US">${
+                  recipe.basics.attenuationLevel
+                }%</p>
+              </div>
+            </div>
+            <div class="cards__method">
+              <h5 class="cards__method--title">Method</h5>
+              <div class="cards__method--details-w">
+                <p class="cards__method--details">Mash Temp</p>
+                <p class="cards__method--details">Fermentation</p>
+              </div>
+              <div class="cards__method--unit-EU-w">
+                <p class="cards__method--unit-EU">${
+                  recipe.method.mashTemp.eu
+                }°C</p>
+                <p class="cards__method--unit-EU">${
+                  recipe.method.fermentation.eu
+                }°C</p>
+              </div>
+              <div class="cards__method--unit-US-w">
+                <p class="cards__method--unit-US">${
+                  recipe.method.mashTemp.us
+                }°F</p>
+                <p class="cards__method--unit-US">${
+                  recipe.method.fermentation.us
+                }°F</p>
+              </div>
+            </div>
+            <div class="cards__ingredients">
+              <h5 class="cards__ingredients--title">Ingredients</h5>
+              <h5 class="cards__ingredients--malt-title">Malt</h5>
+              <div class="cards__ingredients--malt-details-w">
+                 ${recipe.ingredients.malt
+                   .map((malt) => {
+                     return `<p class='cards__ingredients--malt-details'>${malt.name}</p>`;
+                   })
+                   .join("")}
+              </div>
+              <div class="cards__ingredients--malt-unit-EU-w">
+                ${recipe.ingredients.malt
+                  .map((malt) => {
+                    return `
+                  <p class="cards__ingredients--malt-unit-EU">${malt.amount.value}kg</p>
+                  `;
+                  })
+                  .join("")}
+              </div>
+              <div class="cards__ingredients--malt-unit-US-w">
+              ${recipe.ingredients.malt
+                .map((malt) => {
+                  return `
+                <p class="cards__ingredients--malt-unit-US">${(
+                  malt.amount.value * 2.205
+                ).toFixed(2)}lb</p>
+                `;
+                })
+                .join("")}
+             
+              </div>
+              <h5 class="cards__ingredients--hops-title">Hops</h5>
+              <div class="cards__ingredients--hops-details-w">
+              ${recipe.ingredients.hops
+                .map((hops) => {
+                  return `<p class="cards__ingredients--hops-details">${hops.name}</p>`;
+                })
+                .join("")}
+              </div>
+              <div class="cards__ingredients--hops-unit-w">
+                <p class="cards__ingredients--hops-unit-t">g</p>
+                ${recipe.ingredients.hops
+                  .map((hops) => {
+                    return `<p class="cards__ingredients--hops-unit">${hops.amount.value}</p>`;
+                  })
+                  .join("")}
+              </div>
+              <div class="cards__ingredients--hops-add-w">
+                <p class="cards__ingredients--hops-add-t">Add</p>
+                ${recipe.ingredients.hops
+                  .map((hops) => {
+                    return `<p class="cards__ingredients--hops-add">${hops.add
+                      .slice(0, 1)
+                      .toUpperCase()
+                      .concat(hops.add.slice(1))}</p>`;
+                  })
+                  .join("")}
+              </div>
+              <div class="cards__ingredients--hops-attribute-w">
+                <p class="cards__ingredients--hops-attribute-t">Attribute</p>
+                ${recipe.ingredients.hops
+                  .map((hops) => {
+                    return `<p class="cards__ingredients--hops-attribute">${hops.attribute
+                      .slice(0, 1)
+                      .toUpperCase()
+                      .concat(hops.attribute.slice(1))}</p>`;
+                  })
+                  .join("")}
+              </div>
+              <h5 class="cards__ingredients--yeast-title">Yeast</h5>
+              <div class="cards__ingredients--yeast-details-w">
+                <p class="cards__ingredients--yeast-details">
+                  ${recipe.ingredients.yeast}
+                </p>
+              </div>
+            </div>
+            <div class="cards__food">
+              <h5 class="cards__food--title">Food pairing</h5>
+              <p class="cards__food--text">
+                ${food}
+              </p>
+            </div>
+            <div class="cards__image">
+              <img
+                src="${recipe.image}"
+                alt="Beer packaging"
+                class="cards__image--img"
+              />
+            </div>
+            <div class="cards__tip">
+              <h5 class="cards__tip--title">Tip</h5>
+              <p class="cards__tip--text">
+                ${recipe.tip}
+              </p>
+            </div>
+          </div>`;
+
+    recipeContainer.innerHTML = "";
+    recipeContainer.insertAdjacentHTML("afterbegin", markup);
+
+    const maltName = recipe.ingredients.malt
+      .map((malt) => {
+        return `<p class='cards__ingredients--malt-details'>${malt.name}</p>`;
+      })
+      .join("");
+    console.log(maltName);
   } catch (error) {
     console.error(error);
   }
 };
+
 showRecipe();
