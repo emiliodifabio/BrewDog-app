366ab33c4df54e2625b12d053a1fcc43de2c1484
emiliodifabio <emiliodifabio@me.com>
2022-10-13 17:14:11 +0200

    Implemented pagination, scroll to cards, scroll to results

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://api.punkapi.com/v2/beers?";
 export const TIMEOUT_SEC = 10;
-export const AFTER = 01;
-export const BEFORE = 12;
+export const AFTER = '01';
+export const BEFORE = '12';
 export const RES_PER_PAGE = 10;
